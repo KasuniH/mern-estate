37dd27e18c56cb23cd5df7768f9ee8e2775346c0
KasuniHansachapa <e19131@eng.pdn.ac.lk>
2024-09-16 07:41:16 +0530

    create pages and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import About from './pages/About';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-      
+    <BrowserRouter><Routes>
+<Route path="/" element={<Home />}/>
+<Route path="/sign-in" element={<SignIn />}/>
+<Route path="/sign-up" element={<SignUp />}/>
+<Route path="/about" element={<About />}/>
+<Route path="/profile" element={<Profile />}/>
+      </Routes></BrowserRouter>
   )
 }
 
